79ac88765494d134c70202e1c6e34dc5f1bc7403
dcalitri <71895943+dcalitri@users.noreply.github.com>
2022-04-21 21:19:55 -0400

    Got the score to work.

--- Assets/Scripts/BulletCollisions.cs
+++ Assets/Scripts/BulletCollisions.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class BulletCollisions : MonoBehaviour
 {
+    private GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
-
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -17,7 +18,11 @@ public class BulletCollisions : MonoBehaviour
     }
     void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
-        Destroy(other.gameObject);
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            gameManager.UpdateScore(5);
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
     }
 }
